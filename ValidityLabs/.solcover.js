2f2c8f2a4fe1ed649a0444dbf92e12cca585e58a
Yauhen Yakimovich <eugeny.yakimovitch@gmail.com>
2018-05-02 10:00:01 +0200

    Opting for the SC code provided by ValidityLabs.

--- ValidityLabs/.solcover.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    copyNodeModules: true,  //false
-    //copyPackages: ['zeppelin-solidity'],
-    sub: '/src',
-    norpc: false,
-    skipFiles: ['liquidation/LiquidatorInterface.sol','token/MiniMeTokenInterface.sol','token/WETHToken.sol']
-};
